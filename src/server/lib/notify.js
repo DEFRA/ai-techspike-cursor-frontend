9d08dbcdf69d86b15e1a04aff4f6c56f23138913
Steve Dickinson <steve.dickinson@defra.gov.uk>
2025-01-20 18:09:17 +0000

    Add proxy

--- src/server/lib/notify.js
+++ src/server/lib/notify.js
@@ -1,7 +1,18 @@
 import { NotifyClient } from 'notifications-node-client'
 import { config } from '~/src/config/config.js'
+import { provideProxy } from '../common/helpers/proxy.js'
 
 const notifyClient = new NotifyClient(config.get('notifyApiKey'))
+const proxy = provideProxy()
+
+if (proxy) {
+  const proxyConfig = {
+    host: proxy.url,
+    port: proxy.port
+  }
+
+  notifyClient.setProxy(proxyConfig)
+}
 
 export async function sendConfirmationEmail(emailAddress, personalisation) {
   const templateId = config.get('notifyTemplateId')
